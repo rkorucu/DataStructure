b9df1236984354ed68193ed05a9596c2b27dea59
rmznkrc <rmznkrc11@gmail.com>
2022-08-01 15:25:02 -0500

    queue implement

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 
+
 public class Main {
     public static void main(String[] args) {
       //  LinkedList list=new LinkedList();
@@ -11,15 +12,31 @@ public class Main {
 //         list.print();
 
 
-       Stack stack=new Stack();
+//       Stack stack=new Stack();
+//       stack.push(1);
+//       stack.push(2);
+//       stack.push(3);
+//       stack.push(4);
+//       stack.push(5);
+//        System.out.println(stack.size());
 
+//        System.out.println(stack.search(4));
+//        System.out.println(stack.search(9));
+//        System.out.println(stack.peek());
+//      stack.pop();
+//        System.out.println(stack.size());
+//        System.out.println(stack.isEmpty());
+//        System.out.println(stack.size());
 
-        System.out.println(stack.search(4));
-        System.out.println(stack.search(9));
-        System.out.println(stack.peek());
-        System.out.println(stack.pop());
-        System.out.println(stack.isEmpty());
-        System.out.println(stack.size());
+        Queue queue=new Queue();
+        queue.enqueue(2);
+        queue.enqueue(3);
+        queue.enqueue(4);
+        queue.enqueue(5);
+        queue.enqueue(6);
+         queue.print();
+
+        System.out.println(queue.isEmpty());
 
 
 
---
+++ src/Queue.java
@@ -0,0 +1,60 @@
+    public class Queue {
+       Node front;
+        Node rear;
+        int queueSize;
+
+   public Queue(){
+       this.front=null;
+       this.rear=null;
+       this.queueSize=0;
+
+   }
+   public boolean isEmpty(){
+
+      return queueSize==0 ;
+
+   }
+   public void enqueue(int data){
+
+       Node queueNode=new Node(data);
+       queueNode.val=data;
+       queueNode.next=null;
+      if(front==null && rear==null){
+            rear=front=queueNode;
+          System.out.println("Queue is empty");
+          return;
+        }
+       rear.next=queueNode;
+       rear=queueNode;
+       queueSize++;
+
+   }
+   public void  dequeue(){
+
+       if (front==null) return;
+       if (front==rear){
+           front=rear=null;
+       }
+           front=front.next;
+           queueSize--;
+
+   }
+   public int front(){
+       if(isEmpty()) return 0;
+
+    return front.val;
+
+   }
+   public int  queueSize(){
+       System.out.println(queueSize);
+       return queueSize;
+   }
+   public   void  print(){
+       while (front!=null){
+           System.out.println("Queue elements is : " + front.val);
+           front=front.next;
+       }
+
+   }
+
+}
